ad0e8966fa841f7f30b8de2ce97cc4382dd33494
IntrepidTortoise <33159939+Pavlozki@users.noreply.github.com>
2018-03-06 16:55:59 +0000

    Update IMU.cpp

--- IMU.cpp
+++ IMU.cpp
@@ -65,13 +65,39 @@ void IMU::IMU_data_collection(int IMU_identity, MPU6050[] IMUs) {
 }
 
 /// Collect angular rotation data only from IMU
-void IMU::IMU_data_collection(int IMU_identity, MPU6050[] IMUs) {
-    int16_t[3] acc, ang_vel;
-    IMUs[IMU_identity].getRotation(&D.acc[0], &D.acc[1], &D.acc[2], &D.ang_vel[0], &D.ang_vel[1], &D.ang_vel[2]);
+void IMU::IMU_rotational_data_collection(int IMU_identity, MPU6050[] IMUs) {
+    int16_t[3] ang_vel;
+    IMUs[IMU_identity].getMotion(&D.acc[0], &D.acc[1], &D.acc[2], &D.ang_vel[0], &D.ang_vel[1], &D.ang_vel[2]);
 }
 
 /// Collect acceleration data only from IMU
-void IMU::IMU_data_collection(int IMU_identity, MPU6050[] IMUs) {
+void IMU::IMU_acc_data_collection(int IMU_identity, MPU6050[] IMUs) {
     int16_t[3] acc;
     IMUs[IMU_identity].getAcceleration(&D.acc[0], &D.acc[1], &D.acc[2]);
 }
+
+///Collect all data from all IMUs
+void IMU::IMU_general_data_collection(MPU6050[] IMUs){
+    int16_t[9] gen_acc, gen_ang_vel; ///Currently stored as 9 variables in vector, but should be a vector with 3 vectors with 3 int16_t
+    for (int i=0;i<3;i++){
+        IMUs[i].getMotion(&D.gen_acc[0+i*3], &D.gen_acc[1+i*3], &D.gen_acc[2+i*3], &D.gen_ang_vel[0+i*3], &D.gen_ang_vel[1+i*3], &D.gen_ang_vel[2+i*3]);
+    }
+}
+
+///Average data from IMUs
+///First initialise 3 IMUs with identities
+IMU_data IMU0;
+IMU_data IMU1;
+IMU_data IMU2;
+
+IMU0.IMU_identity = 0;
+IMU1.IMU_identity = 1;
+IMU2.IMU_identity = 2;
+
+void IMU::IMU_average_data(MPU6050[] IMUs){
+    ///Check how averaging works for bit-numbers, and if values need converting
+}    
+///Check for error in data from IMUs
+
+        
+        
